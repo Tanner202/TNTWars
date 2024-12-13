da646b599958ef50ba3c30fecf8c72cbec1a652c
Tanner <tannerelephant@gmail.com>
2024-12-13 09:21:00 -0800

    remove broadcast message when game starts

--- src/main/java/com/tanner/tntwars/listener/ArenaListener.java
+++ src/main/java/com/tanner/tntwars/listener/ArenaListener.java
@@ -5,7 +5,6 @@ import com.tanner.minigames.event.GameStartEvent;
 import com.tanner.minigames.instance.Arena;
 import com.tanner.tntwars.TNTWars;
 import com.tanner.tntwars.instance.Game;
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -24,7 +23,6 @@ public class ArenaListener implements Listener {
     @EventHandler
     public void onGameStart(GameStartEvent e) {
         game = new Game(arena, tntWars);
-        Bukkit.broadcastMessage("TNTWARS: on game start");
     }
 
     @EventHandler
